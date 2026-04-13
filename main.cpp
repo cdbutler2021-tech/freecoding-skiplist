59df7ad4a7d73be9a8561ad4161b865d45ff3f91
loaner <cdbutler2021@gmail.com>
2026-04-13 13:34:35 -0600

    trimmed down the main file

--- main.cpp
+++ main.cpp
@@ -5,32 +5,10 @@
 
 using namespace std;
 
-SkipList<int> random_list(int min, int max, int n) {
-    SkipList<int> sl;
-    for (int i=0; i<n; i++) {
-        std::random_device rnd;
-        std::mt19937 genr(rnd());
-        std::uniform_int_distribution<> dist(min,max);
-        int const result = dist(genr);
-        sl.insert(result);
-    }
-    return sl;
-}
-
-SkipList<int> sequential_list(int start, int n) {
-    SkipList<int> sl;
-    for (int i=start; i<start+n; i++) {
-        sl.insert(i);
-    }
-    return sl;
-}
-
 int main() {
     SkipList<int> my_sl;
     for (int i=0; i<8; i++) {
         my_sl.insert(i);
     }
-    my_sl.print();
-
     return 0;
 }
